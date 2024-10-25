b67ed7f94238157827acfa2ecc8ac5e08b06f3e1
Angad <angadkumar70676@gmail.com>
2024-10-26 04:14:57 +0530

    frontend deployed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173','https://todo-app-react-js-nine.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
 }));
